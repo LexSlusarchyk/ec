ec137ef401d681ffbff19a3afaa6b2776fb256ff
LexSlusarchyk <lexslusarchyk@gmail.com>
2018-01-29 16:53:45 +0200

    add ui-router and first controller

--- app/app.js
+++ app/app.js
@@ -1,14 +1,18 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+angular
+    .module('myApp', [
+      'ngRoute',
+      'ui.router',
+      'ui.router.state.events',
+      'myApp.view1',
+      'myApp.view2',
+      'myApp.version'
+])
+    .config(['$urlRouterProvider', '$locationProvider', '$routeProvider',
+        function($urlRouterProvider, $locationProvider, $routeProvider) {
+            $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+ // $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
---
+++ app/home-state/home.controller.js
@@ -0,0 +1,19 @@
+(function () {
+    'use strict';
+    angular
+        .module('myApp')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = [];
+
+    function HomeController() {
+        var vm = this;
+
+
+
+
+
+    }
+})();
+
+
